0b4ae158344a682ae2a57e153acd068b2048d069
Karlson Thien <prolexicgt@gmail.com>
2024-10-07 08:55:12 +0800

    Updated code

--- public/categories/stationary.php
+++
@@ -1,3 +0,0 @@
-<?php require_once($root . 'templates/header.php'); ?>
-    <p>This is where stationary product display template will be</p>
-<?php require_once($root . 'templates/footer.php'); ?>
--- public/pages/404.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once($root . 'templates/header.php');
-echo "Oops not found";
-require_once($root . 'templates/footer.php');
