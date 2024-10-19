086a09a722f06f657d31451a49bbf8053ae07ff7
Java <yewshan.ooi@gmail.com>
2024-10-19 13:38:44 +0800

    feat: added product name, refined product image and description

--- public/category/clothing.php
+++ public/category/clothing.php
@@ -9,28 +9,28 @@
 	</div>
 	<div class="item-list-preview" id="container">
 		<div class="product">
-			<div class="product-image">
-				<p>Image</p>
-				<img src="/src/store/hat.png" alt="Hat">
+			<div class="product-name">
+				<h3>Hat</h3>
 			</div>
+			<img src="/src/store/hat.png" alt="Hat" class="product-image">
 			<div class="product-desc">
 				<p>Step out in style with APU Hat™. Designed for students who want to look good while making a positive impact on the planet. Perfect for outdoor adventures, casual outings, or simply keeping the sun at bay. Hat is made using 80% recycled materials.</p>
 			</div>
 		</div>
 		<div class="product">
-			<div class="product-image">
-				<p>Image</p>
-				<img src="/src/store/necktie.png" alt="Neck Tie">
+			<div class="product-name">
+				<h3>Neck Tie</h3>
 			</div>
+			<img src="/src/store/necktie.png" alt="Neck Tie" class="product-image">
 			<div class="product-desc">
-				<p>Elevate your style with APU Necktie™. Whether for a formal event or a special occasion, this tie adds a touch of class while promoting sustainable practices. Material is made using 70% recycled fabrics sourced from local farmers.</p>
+				<p>Elevate your style with APU Neck Tie™. Whether for a formal event or a special occasion, this tie adds a touch of class while promoting sustainable practices. Material is made using 70% recycled fabrics sourced from local farmers.</p>
 			</div>
 		</div>
 		<div class="product">
-			<div class="product-image">
-				<p>Image</p>
-				<img src="/src/store/tshirt.png" alt="T Shirt">
+			<div class="product-name">
+				<h3>T-Shirt</h3>
 			</div>
+			<img src="/src/store/tshirt.png" alt="T Shirt" class="product-image">
 			<div class="product-desc">
 				<p>Experience ultimate comfort and style with APU T-Shirt™. Designed for students who value fashion and sustainability. Perfect for everyday wear, this versatile tee is a must-have for any wardrobe. T-Shirt material is made using sustainably sourced cotton.</p>
 			</div>
--- public/category/stationary.php
+++ public/category/stationary.php
@@ -9,28 +9,28 @@
 	</div>
 	<div class="item-list-preview" id="container">
 		<div class="product">
-			<div class="product-image">
-				<p>Image</p>
-				<img src="/src/store/book.png" alt="Book">
+			<div class="product-name">
+				<h3>Book</h3>
 			</div>
+			<img src="/src/store/book.png" alt="Book" class="product-image">
 			<div class="product-desc">
 				<p>Unleash your creativity and stay organized with APU Book™. The perfect companion for students who loves to jot down thoughts, ideas, and dreams. This notebook is as good for the planet as it is for your mind.</p>
 			</div>
 		</div>
 		<div class="product">
-			<div class="product-image">
-				<p>Image</p>
-				<img src="/src/store/calculator.png" alt="Calculator">
+			<div class="product-name">
+				<h3>Calculator</h3>
 			</div>
+			<img src="/src/store/calculator.png" alt="Calculator" class="product-image">
 			<div class="product-desc">
 				<p>Be efficient with APU Calculator™. The perfect tool for students who require a maths companion. This calculator not only helps you crunch numbers but also supports eco-friendly practices.</p>
 			</div>
 		</div>
 		<div class="product">
-			<div class="product-image">
-				<p>Image</p>
-				<img src="/src/store/pen.png" alt="Pen">
+			<div class="product-name">
+				<h3>Pen</h3>
 			</div>
+			<img src="/src/store/pen.png" alt="Pen" class="product-image">
 			<div class="product-desc">
 				<p>Elevate your note-taking game with APU Pen™. Designed specifically for modern university student. Manufactured with sustainable materials, this pen delivers a smooth, reliable writing experience at the same time reflects your commitment to the planet.</p>
 			</div>
